5fbad88fddb67542908de46ecf2d877c1cfff43a
Chuy-er <Chihuy764@gmail.com>
2024-10-08 08:59:32 +0700

    Thêm Trang mua cá

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import SearchPage from './SearchPage';
 import ConsignmentPage from './ConsignmentPage';
 import FeedbackPage from './FeedbackPage';  
 import CustomerProfilePage from './CustomerProfilePage'; 
+import BuyFishPage from './BuyFishPage';
+import ShoppingFish from './ShoppingFish';
 
 const App = () => {
     const [page, setPage] = useState('search');
@@ -12,6 +14,7 @@ const App = () => {
         <div>
             <nav>
                 <button onClick={() => setPage('search')}>Trang Tìm Kiếm Cá Koi</button>
+                <button onClick={() => setPage('buyFish')}>Trang Mua Cá</button> 
                 <button onClick={() => setPage('consignment')}>Trang Ký Gửi Cá Koi</button>
                 <button onClick={() => setPage('feedback')}>Trang Đánh Giá và Phản Hồi</button>
                 <button onClick={() => setPage('profile')}>Trang Hồ Sơ Khách Hàng</button> 
@@ -21,6 +24,8 @@ const App = () => {
                 {page === 'consignment' && <ConsignmentPage />}
                 {page === 'feedback' && <FeedbackPage />}
                 {page === 'profile' && <CustomerProfilePage />} 
+                {page === 'buyFish' && <BuyFishPage />} 
+                {page === 'shoppingFish' && <ShoppingFish />} 
             </div>
             <div className="botset-container">
                 <img src="https://onkoi.vn/wp-content/themes/themenamewoo/images/bg_botsec1.png" alt="Botset" />
